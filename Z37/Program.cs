19f1ae5fc223069022e97037b56b47a13565e325
Андрей Левченков <lev_a_a@mail.ru>
2024-07-17 20:27:17 +0200

    Z37

---
+++ Z37/Program.cs
@@ -0,0 +1,6 @@
+﻿using Zadanie37;
+
+Geometry geometry = new Geometry(4, 5);
+Console.WriteLine($"Площадь прямоугольника: {geometry.squreArea()}");
+Console.WriteLine($"Пермиретр прямоуголька: {geometry.perimeterlArea()}");
+Console.WriteLine($"Диагональ прямоугольника: {geometry.diagonalArea():F2}");
