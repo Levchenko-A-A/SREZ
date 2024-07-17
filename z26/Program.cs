af4262d2e2482f30d136b16e3366d1b7777856bc
Marin2421 <vladlan071997@gmail.com>
2024-07-17 21:10:35 +0200

    z26

---
+++ z26/Program.cs
@@ -0,0 +1,21 @@
+﻿int[,] mas = new int[5, 5];
+Random random = new Random();
+
+for (int i = 0; i < mas.GetLength(0); i++)
+{
+    for (int j = 0; j < mas.GetLength(1); j++)
+    {
+        mas[i, j] = random.Next(-10, 11);
+        Console.Write(mas[i, j]+" ");
+    }
+    Console.WriteLine();
+}
+for (int i = 0;i < mas.GetLength(0); i++)
+{
+    int sum = 0;
+    for (int j = 0;j < mas.GetLength(1); j++)
+    {
+        sum += mas[i, j];       
+    }
+    Console.WriteLine(sum);
+}
