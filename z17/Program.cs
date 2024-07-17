36885f0323cd3ebe97da32d5f1fde60cd1a36f91
Marin2421 <vladlan071997@gmail.com>
2024-07-17 20:51:25 +0200

    z17

---
+++ z17/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите слово: ");
+string slovo=Console.ReadLine()!;
+string alfavit = "йцукенгшщзхъэждлорпавыфячсмитьбюё";
+int count=0;
+for(int i = 0; i < alfavit.Length; i++)
+{
+    for(int j = 0; j < slovo.Length; j++)
+    {
+        if (alfavit[i] == slovo[j]) {count++;break;}
+
+    }
+} 
+if (count == 33) Console.WriteLine("Слово панграмма");
+else Console.WriteLine("Слово не является панграммой");
