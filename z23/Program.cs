55d0c87f012bcfd7c572e05748ef4eafec79963a
Marin2421 <vladlan071997@gmail.com>
2024-07-17 21:00:32 +0200

    z23

---
+++ z23/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] mas = new int[8] { -8, -4, -2, -1, 0, 2, 5, 6 };
+int min=int.MaxValue;
+int max=int.MinValue;
+for(int i = 0; i < mas.Length; i++)
+{
+    if (mas[i] < min) min = mas[i];
+    if (mas[i] > max) max = mas[i];
+}
+Console.Write($"Минимальное число: {min} Максимальное число: {max}");
