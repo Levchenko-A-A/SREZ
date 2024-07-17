1b19a7de264524643aeabb20757fc877d3340724
Marin2421 <vladlan071997@gmail.com>
2024-07-17 20:36:16 +0200

    z14

---
+++ z14/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int n=int.Parse(Console.ReadLine()!);
+if(CheckNumber(n)) Console.WriteLine($"{n} является совершенным числом");
+else Console.WriteLine($"{n} не является совершенным числом");
+bool CheckNumber(int n)
+{
+    int sum = 1;
+    for(int i = 2;i<n/2+1;i++) if(n%i==0) sum+=i;
+    return (sum == n);
+}
